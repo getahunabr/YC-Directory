07ad3a200892525bdc7a4a6635f69184e730a4bf
getahunabr <147873183+getahunabr@users.noreply.github.com>
2025-04-17 13:52:51 -0700

    validation

--- lib/Validation.ts
+++ lib/Validation.ts
@@ -1,15 +1,12 @@
 import { z } from "zod";
 
 export const formSchema = z.object({
-  title: z.string().min(3, "Title must be at least 3 characters").max(100),
-  description: z
-    .string()
-    .min(20, "Description must be at least 20 characters")
-    .max(500),
-  category: z.string().min(3, "Category must be at least 3 characters").max(20),
+  title: z.string().min(3).max(100),
+  description: z.string().min(20).max(500),
+  category: z.string().min(3).max(20),
   link: z
     .string()
-    .url()
+    .url("Must be a valid image URL")
     .refine(
       async (url) => {
         try {
@@ -20,8 +17,9 @@ export const formSchema = z.object({
           return false;
         }
       },
-      
+      {
+        message: "URL must point to a valid image",
+      }
     ),
-
-  pitch: z.string().min(10, "Pitch must be at least 10 characters").max(1000),
+  pitch: z.string().min(10).max(1000),
 });
